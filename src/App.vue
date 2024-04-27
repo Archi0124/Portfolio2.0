8e11532c42e891caf7df865fdfbd0ae58c187587
a-nosikov <a-nosikov@ALKONA-NB023>
2024-04-27 11:07:21 +0300

    edit: Добавил секции по структуре платформы

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,24 @@
 <script setup>
 import TheHeader from './components/TheHeader.vue';
+import TheHeaderBlogInfo from './components/TheHeaderBlogInfo.vue';
+import TheSectionWorkStudent from './components/TheSectionWorkStudent.vue';
+import TheSectionTeam from './components/TheSectionTeam.vue';
+import TheSectionTrend from './components/TheSectionTrend.vue';
+import TheSectionRankUsers from './components/TheSectionRankUsers.vue';
+import TheSectionAuthors from './components/TheSectionAuthors.vue';
+import TheSectionTasks from './components/TheSectionTasks.vue';
+import TheSectionFooter from './components/TheSectionFooter.vue';
 </script>
 <template>
   
   <TheHeader />
+  <TheHeaderBlogInfo/>
+  <TheSectionWorkStudent/>
+  <TheSectionTeam/>
+  <TheSectionTrend/>
+  <TheSectionRankUsers/>
+  <TheSectionAuthors/>
+  <TheSectionTasks/>
+  <TheSectionFooter/>
 </template>
 
